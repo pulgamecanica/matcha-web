72d2e8d1335c05d1d8c8844aa491c5c3eaf11957
pulgamecanica <pulgamecanica11@gmail.com>
2025-04-10 12:31:31 +0200

    enable darkmode for not found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -4,10 +4,10 @@ export const NotFoundPage = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen text-center px-4">
             <h1 className="text-7xl font-extrabold text-red-500">404</h1>
-            <h2 className="text-2xl font-semibold text-gray-800 mt-4">
+            <h2 className="text-2xl font-semibold text-gray-800 dark:text-gray-300 mt-4">
                 Page Not Found
             </h2>
-            <p className="text-gray-600 mt-2">
+            <p className="text-gray-600 dark:text-gray-100 mt-2">
                 Sorry, the page you're looking for doesn't exist or has been moved.
             </p>
             <Link
